84ec798e19b89b00812f650cbc9e6cd1ab635d6e
diodax <diosdavi20@gmail.com>
2016-04-05 20:55:48 -0400

    semi fixed el problema de los modals

--- ProyectoMPLS/Scripts/modalform.js
+++ ProyectoMPLS/Scripts/modalform.js
@@ -6,7 +6,26 @@
 
         // hide dropdown if any
         $(e.target).closest('.btn-group').children('.dropdown-toggle').dropdown('toggle');
-
+        
+        
+        var modalSizeClass = $(this).attr("class");
+        // extracts the selector class to determine the modals size
+        
+        switch (modalSizeClass) {
+            case 'size-sm':
+                $("#myModal").removeAttr("data-width");
+                break;
+            case 'size-lg':
+                //sets data-width="860"
+                $("#myModal").attr("data-width", "860");
+                break;
+            default:
+                //default size
+                //$("#myModal").attr("data-width", "660");
+                $("#myModal").removeAttr("data-width");
+        }
+        
+        
 
         $('#myModalContent').load(this.href, function () {
 
