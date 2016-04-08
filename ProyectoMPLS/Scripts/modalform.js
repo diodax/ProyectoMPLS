6f38d6944988187e3acf4ab64e7a2e0d9c160b0e
Victor Rosario <vrosario93@gmail.com>
2016-04-08 17:23:41 -0400

    Modal en stack con bugs

--- ProyectoMPLS/Scripts/modalform.js
+++ ProyectoMPLS/Scripts/modalform.js
@@ -7,36 +7,72 @@
         // hide dropdown if any
         $(e.target).closest('.btn-group').children('.dropdown-toggle').dropdown('toggle');
         
-        
-        var modalSizeClass = $(this).attr("class");
-        // extracts the selector class to determine the modals size
-        
-        switch (modalSizeClass) {
-            case 'size-sm':
-                $("#myModal").removeAttr("data-width");
-                break;
-            case 'size-lg':
-                //sets data-width="860"
-                $("#myModal").attr("data-width", "860");
-                break;
-            default:
-                //default size
-                //$("#myModal").attr("data-width", "660");
-                $("#myModal").removeAttr("data-width");
+        var modId = $(this).attr("id");
+        console.log(modId);
+
+        if (modId == "linkEditarAfinidad") {
+            var modalSizeClass = $(this).attr("class");
+            // extracts the selector class to determine the modals size
+
+            switch (modalSizeClass) {
+                case 'size-sm':
+                    $("#afinidad").removeAttr("data-width");
+                    break;
+                case 'size-lg':
+                    //sets data-width="860"
+                    $("#afinidad").attr("data-width", "860");
+                    break;
+                default:
+                    //default size
+                    //$("#myModal").attr("data-width", "660");
+                    $("#afinidad").removeAttr("data-width");
+            }
+
+
+
+            $('#afinidadContent').load(this.href, function () {
+
+
+                $('#afinidad').modal({
+                    /*backdrop: 'static',*/
+                    keyboard: true
+                }, 'show');
+
+                bindForm(this);
+            });
+        }
+        else {
+            var modalSizeClass = $(this).attr("class");
+            // extracts the selector class to determine the modals size
+
+            switch (modalSizeClass) {
+                case 'size-sm':
+                    $("#myModal").removeAttr("data-width");
+                    break;
+                case 'size-lg':
+                    //sets data-width="860"
+                    $("#myModal").attr("data-width", "860");
+                    break;
+                default:
+                    //default size
+                    //$("#myModal").attr("data-width", "660");
+                    $("#myModal").removeAttr("data-width");
+            }
+
+
+
+            $('#myModalContent').load(this.href, function () {
+
+
+                $('#myModal').modal({
+                    /*backdrop: 'static',*/
+                    keyboard: true
+                }, 'show');
+
+                bindForm(this);
+            });
         }
         
-        
-
-        $('#myModalContent').load(this.href, function () {
-
-
-            $('#myModal').modal({
-                /*backdrop: 'static',*/
-                keyboard: true
-            }, 'show');
-
-            bindForm(this);
-        });
 
         return false;
     });
@@ -53,12 +89,26 @@ function bindForm(dialog) {
             data: $(this).serialize(),
             success: function (result) {
                 if (result.success) {
-                    $('#myModal').modal('hide');
-                    //Refresh
-                    location.reload();
+                    if ($(dialog).attr("id") == "linkEditarAfinidad") {
+                        console.log("entro al success");
+                        $('#afinidad').modal('hide');
+                        //Refresh
+                        location.reload();
+                    }
+                    else {
+                        $('#myModal').modal('hide');
+                        //Refresh
+                        location.reload();
+                    }
                 } else {
-                    $('#myModalContent').html(result);
-                    bindForm();
+                    if ($(dialog).attr("id") == "linkEditarAfinidad") {
+                        $('#afinidadContent').html(result);
+                        bindForm();
+                    }
+                    else {
+                        $('#myModalContent').html(result);
+                        bindForm();
+                    }
                 }
             }
         });
