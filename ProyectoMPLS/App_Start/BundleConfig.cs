22ceb020ae4a6ae94303240c125399a077eac102
diodax <diosdavi20@gmail.com>
2016-04-15 03:11:45 -0400

    Cambio de Template a Minton. Rediseño parcial del Layout básico y el Layout de login

--- ProyectoMPLS/App_Start/BundleConfig.cs
+++ ProyectoMPLS/App_Start/BundleConfig.cs
@@ -9,7 +9,15 @@ namespace ProyectoMPLS
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/bower_components/jquery/dist/jquery.js"));
+                        "~/assets/js/jquery.min.js",
+                        "~/assets/js/bootstrap.min.js",
+                        "~/assets/js/detect.js",
+                        "~/assets/js/fastclick.js",
+                        "~/assets/js/jquery.blockUI.js",
+                        "~/assets/js/waves.js",
+                        "~/assets/js/wow.min.js", 
+                        "~/assets/js/jquery.nicescroll.js",
+                        "~/assets/js/jquery.scrollTo.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -17,22 +25,25 @@ namespace ProyectoMPLS
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/assets/js/modernizr.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/bower_components/bootstrap/dist/js/bootstrap.js",
-                        "~/bower_components/bootstrap-modal/js/bootstrap-modalmanager.js",
-                        "~/bower_components/bootstrap-modal/js/bootstrap-modal.js",
-                        "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //            "~/bower_components/bootstrap/dist/js/bootstrap.js",
+            //            "~/bower_components/bootstrap-modal/js/bootstrap-modalmanager.js",
+            //            "~/bower_components/bootstrap-modal/js/bootstrap-modal.js",
+            //            "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/bower_components/bootstrap/dist/css/bootstrap.css",
-                        "~/bower_components/bootstrap-modal/css/bootstrap-modal-bs3patch.css",
-                        "~/bower_components/bootstrap-modal/css/bootstrap-modal.css",
-                        "~/Content/site.css"));
+                        "~/assets/plugins/jquery-circliful/css/jquery.circliful.css",
+                        "~/assets/css/bootstrap.min.css",
+                        "~/assets/css/core.css",
+                        "~/assets/css/components.css",
+                        "~/assets/css/icons.css",
+                        "~/assets/css/pages.css",
+                        "~/assets/css/menu.css",
+                        "~/assets/css/responsive.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/modalform").Include(
-                        "~/Scripts/modalform.js"));
+            
         }
     }
 }
